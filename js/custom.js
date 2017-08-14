9cce7426f29984af940d6a10f0762f1e23cdb20c
Jason Vance <jbvance@gmail.com>
2017-08-14 14:31:15 -0500

    Complete testimonials section

--- js/custom.js
+++ js/custom.js
@@ -22,11 +22,25 @@ $(function () {
 
 
 /*================================================================
-                    WORK
+                   TEAM
 ================================================================*/
 $(function () {
     $("#team-members").owlCarousel({
         items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+
+
+/*================================================================
+                  TESTIMONIALS
+================================================================*/
+$(function () {
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
