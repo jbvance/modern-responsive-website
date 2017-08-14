94a33505445d80b00d36f9e6f4f4dd9874ea6c29
Jason Vance <jbvance@gmail.com>
2017-08-14 11:52:20 -0500

    Complete Team section

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,17 @@ $(function () {
         }
     });
 });
+
+
+/*================================================================
+                    WORK
+================================================================*/
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
