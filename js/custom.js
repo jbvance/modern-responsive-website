8a5475b7cc11c125204714ea3f6e38a488880f05
Jason Vance <jbvance@gmail.com>
2017-08-14 09:55:43 -0500

    Complete Work section

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,16 @@ $(function () {
     // animate on scroll
     new WOW().init();
 });
+
+/*================================================================
+                    WORK
+================================================================*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', //child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
